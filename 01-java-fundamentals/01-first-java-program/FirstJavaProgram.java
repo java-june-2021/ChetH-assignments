ace200ebead3b35d23c19f760dc570a75e223517
chuang-ds <chet.huang.vt@gmail.com>
2021-06-07 21:26:48 -0700

    assignment 1

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Chet Huang");
+        System.out.println("I am 50 years old");
+        System.out.println("My hometown is Seattle, WA");
+    }
+}
