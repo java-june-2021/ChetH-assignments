90cc50b5e0862f0a6371568114949ae03794f77b
chuang-ds <chet.huang.vt@gmail.com>
2021-06-23 00:24:36 -0700

    assignment 2

--- 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/src/main/java/com/example/demo/controllers/DojoController.java
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/src/main/java/com/example/demo/controllers/DojoController.java
@@ -1,29 +1,25 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="dojo")
-//@RequestMapping(value="/dojo/{location}")
 
 public class DojoController {
-//	public String location(@PathVariable("location") String dojolocation) {
-//	    if(dojolocation.equals("")) {
-//	    	return "The dojo is awesome!";
-//	    }
-//	    if(dojolocation.equals("burbank")) {
-//	    	return "Burbank Dojo is in Southern California";
-//	    }
-//	    if(dojolocation.equals("san-jose")) {
-//	    	return "SJ dojo is the headquarters";
-//	    }
-//	    return "";
-//	}
-	@RequestMapping(value="burbank", method=RequestMethod.GET)
-	public String location() {
-		return "This sucks";
+	@RequestMapping(value="/dojo")
+	public String dojo() {
+		return "The dojo is awesome!";
 	}
 	
+	@RequestMapping(value="/dojo/{loc}")
+	public String location(@PathVariable("loc") String dojolocation) {
+	    if(dojolocation.equals("burbank")) {
+	    	return "Burbank Dojo is in Southern California";
+	    }
+	    else if(dojolocation.equals("san-jose")) {
+	    	return "SJ dojo is the headquarters";
+	    }
+	    else { return "Look for future expansion of dojo";}
+	}
 }
